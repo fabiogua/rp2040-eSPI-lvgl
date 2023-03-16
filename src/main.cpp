c07d6e628365acc5b40edff2f876f10a19420dce
Fabio Guarino <fabio@guarino.de>
2023-03-16 11:59:47 +0100

    test CAN-Bus

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 #include <lvgl.h>
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
+#include <FlexCAN.h>
+#include <kinetis_flexcan.h>
+
+FlexCAN CANReceiver(100000);
+CAN_message_t msg;
 
 /*Change to your screen resolution*/
-static const uint16_t screenWidth = 480;
-static const uint16_t screenHeight = 320;
+static const uint16_t screenWidth = 320;
+static const uint16_t screenHeight = 240;
 
 #define TFT_BL 13
 uint8_t TftBackground = 150;
@@ -22,6 +27,9 @@ uint16_t rawPointMaxY = 3700;
 uint16_t screenPointX;
 uint16_t screenPointY;
 lv_obj_t *labelPoint;
+lv_obj_t *labelCurrent;
+lv_obj_t *labelVoltage;
+lv_obj_t *labelSpeed;
 lv_indev_t *mouse_indev;
 lv_obj_t *cursor_obj;
 lv_indev_drv_t indev_drv;
@@ -30,13 +38,14 @@ lv_obj_t *roller1;
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
 
-#define CS_PIN 17
+#define CS_PIN 8
+#define TIRQ_PIN 2
 
 // XPT2046_Touchscreen ts(CS_PIN);
 // #define TIRQ_PIN 22
-XPT2046_Touchscreen ts(CS_PIN); // Param 2 - NULL - No interrupts
+// XPT2046_Touchscreen ts(CS_PIN); // Param 2 - NULL - No interrupts
 // XPT2046_Touchscreen ts(CS_PIN, 255);  // Param 2 - 255 - No interrupts
-// XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN); // Param 2 - Touch IRQ Pin - interrupt enabled polling
+XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN); // Param 2 - Touch IRQ Pin - interrupt enabled polling
 
 #if LV_USE_LOG != 0
 /* Serial debugging */
@@ -72,33 +81,32 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 
   lv_disp_flush_ready(disp);
 }
+
 void touch_xpt2046_init(void)
 {
   ts.begin();
   ts.setRotation(3);
 }
-/*Read the touchpad*/
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
   if (ts.touched())
   {
     data->state = LV_INDEV_STATE_PR;
     TS_Point p = ts.getPoint();
-    /*Set the coordinates*/
 
     screenPointX = screenWidth * (p.x - rawPointMinX) / (rawPointMaxX - rawPointMinX);
     screenPointY = screenHeight * (p.y - rawPointMinY) / (rawPointMaxY - rawPointMinY);
 
     data->point.x = screenPointX;
     data->point.y = screenPointY;
-    // data->point.x = p.x;
-    // data->point.y = p.y;
+    data->point.x = p.x;
+    data->point.y = p.y;
 
-    // Serial.print("touch x ");
-    // Serial.print(p.x);
+    Serial.print("touch x ");
+    Serial.print(p.x);
 
-    // Serial.print(", touch y ");
-    // Serial.println(p.y);
+    Serial.print(", touch y ");
+    Serial.println(p.y);
   }
   else
   {
@@ -108,6 +116,9 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 
 void setup()
 {
+  CANReceiver.begin();
+  delay(1000);
+  Serial.println("CAN Receiver Initialized");
 
   /* tft backgroud lighting setting */
   pinMode(TFT_BL, OUTPUT);
@@ -149,7 +160,7 @@ void setup()
   lv_indev_drv_init(&indev_drv);
   indev_drv.type = LV_INDEV_TYPE_POINTER;
   indev_drv.read_cb = my_touchpad_read;
-  // lv_indev_drv_register(&indev_drv);
+  lv_indev_drv_register(&indev_drv);
 
   /* add a cursor */
   mouse_indev = lv_indev_drv_register(&indev_drv);
@@ -164,28 +175,67 @@ void setup()
   lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
   labelPoint = lv_label_create(lv_scr_act());
-  lv_label_set_text_fmt(labelPoint, "Point:%d, %d", screenPointX, screenPointY);
-  lv_obj_align(labelPoint, LV_ALIGN_CENTER, 0, 50);
+  lv_obj_align(labelPoint, LV_ALIGN_CENTER, 0, 20);
 
-  roller1 = lv_roller_create(lv_scr_act());
-  lv_roller_set_options(roller1,
-                        "Welcome\n"
-                        "My\n"
-                        "Program",
-                        LV_ROLLER_MODE_INFINITE);
+  labelCurrent = lv_label_create(lv_scr_act());
+  lv_obj_align(labelCurrent, LV_ALIGN_CENTER, 0, 40);
 
-  lv_roller_set_visible_row_count(roller1, 4);
-  lv_obj_center(roller1);
-  lv_obj_add_event_cb(roller1, event_handler, LV_EVENT_ALL, NULL);
-  lv_obj_align(roller1, LV_ALIGN_LEFT_MID, 10, 0);
+  labelVoltage = lv_label_create(lv_scr_act());
+  lv_obj_align(labelVoltage, LV_ALIGN_CENTER, 0, 60);
+
+  labelSpeed = lv_label_create(lv_scr_act());
+  lv_obj_align(labelSpeed, LV_ALIGN_CENTER, 0, 80);
 
   Serial.println("Setup done");
 }
 
 void loop()
 {
-  lv_label_set_text_fmt(labelPoint, "%d, %d", screenPointX, screenPointY);
-  // lv_indev_set_cursor(mouse_indev, cursor_obj); /*Connect the image  object to the driver*/
+  while (CANReceiver.read(msg))
+  {
+    // toggle LEDs
+    if (msg.buf[0] == 16)
+    {
+      if (msg.buf[1] == 0)
+      {
+        Serial.print("Status ");
+        Serial.println(msg.buf[2]);
+
+        Serial.print("State ");
+        Serial.println(msg.buf[3]);
+
+        uint16_t speed16 = ((uint16_t)msg.buf[4] << 8) | (uint16_t)msg.buf[5];
+        float speed = (float)speed16 / 100.0;
+        lv_label_set_text_fmt(labelSpeed, "Speed: %.2f km/h", speed);
+
+        // this->setSpeed(speed);
+        Serial.print("Speed ");
+        Serial.print(speed);
+        Serial.println("km/h");
+
+        uint16_t current16 = ((uint16_t)msg.buf[6] << 8) | (uint16_t)msg.buf[7];
+        float current = (float)current16 / 100.0;
+        lv_label_set_text_fmt(labelCurrent, "Current: %.2f A", current);
+        // this->setCurrent(current);
+        Serial.print("Current ");
+        Serial.print(current);
+        Serial.println("A");
+      }
+      else if (msg.buf[1] == 1)
+      {
+        Serial.print("MotorTemp ");
+        Serial.println(msg.buf[2]);
+
+        Serial.print("AirTemp ");
+        Serial.println(msg.buf[3]);
+
+        uint16_t dcVoltage16 = ((uint16_t)msg.buf[4] << 8) | (uint16_t)msg.buf[5];
+        float dcVoltage = (float)dcVoltage16 / 100.0;
+        lv_label_set_text_fmt(labelVoltage, "Voltage: %.2f V", dcVoltage);
+      }
+    }
+  }
+  lv_label_set_text_fmt(labelPoint, "Point: %d, %d", screenPointX, screenPointY);
   lv_timer_handler(); /* let the GUI do its work */
   delay(10);
 }
